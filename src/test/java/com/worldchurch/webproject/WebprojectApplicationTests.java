7d3a37dba9a36d0629299c206712698104ef08c8
lee seonguk <passion1014@gmail.com>
2022-05-20 00:43:18 +0900

    하이! 친구들

---
+++ src/test/java/com/worldchurch/webproject/WebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldchurch.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
