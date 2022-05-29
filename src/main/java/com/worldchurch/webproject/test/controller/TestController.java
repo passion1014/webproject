db59c48fca76e573a81bd30038cc4eec65e38775
lee seonguk <passion1014@gmail.com>
2022-05-29 21:18:59 +0900

    hello world working

--- src/main/java/com/worldchurch/webproject/test/controller/TestController.java
+++ src/main/java/com/worldchurch/webproject/test/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String hello() {
-        return "hello";
+        return "hello world";
     }
 }
