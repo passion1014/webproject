a9354eb9dcf698e4bbb1127846e828a754d390bd
poohoh <kjh980328@gmail.com>
2023-01-07 14:20:38 +0900

    TestController, hello.html

--- src/main/java/com/worldchurch/webproject/test/controller/TestController.java
+++ src/main/java/com/worldchurch/webproject/test/controller/TestController.java
@@ -1,14 +1,11 @@
 package com.worldchurch.webproject.test.controller;
 
 import com.worldchurch.webproject.test.service.HelloWorldService;
-import com.worldchurch.webproject.test.service.HelloWorldServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -29,5 +26,14 @@ public class TestController {
 
         attributes.addFlashAttribute("flashAttr", "이성욱");
         return "redirect:/test/hello";
+    }
+
+    @GetMapping("/test/{name1}/{name2}")
+    public String hello(@PathVariable("name1") String name1, @PathVariable("name2") String name2, Model model) {
+        // return helloWorldService.getGreeting(name);
+        model.addAttribute("data", name1);
+        model.addAttribute("data1", name2);
+
+        return "test/hello";
     }
 }
